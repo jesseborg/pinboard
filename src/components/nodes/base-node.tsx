6476e5ea23873b2218816acdacf9685e09884dd0
jesseborg <6882782+jesseborg@users.noreply.github.com>
2024-02-27 17:21:10 +1000

    style(nodes): add className appending to `BaseNode`

--- src/components/nodes/base-node.tsx
+++ src/components/nodes/base-node.tsx
@@ -1,12 +1,21 @@
-import { PropsWithChildren } from "react";
+import { cn } from "@/lib/utils";
+import { HTMLAttributes, PropsWithChildren } from "react";
 
 type BaseNodeProps = {
 	/* ... */
 };
 
-export function BaseNode({ children }: PropsWithChildren<BaseNodeProps>) {
+export function BaseNode({
+	className,
+	children,
+}: PropsWithChildren<BaseNodeProps & HTMLAttributes<HTMLDivElement>>) {
 	return (
-		<div className="border-2 border-black p-2 bg-white shadow-[2px_2px] shadow-black">
+		<div
+			className={cn(
+				"border-2 border-black p-2 bg-white shadow-[2px_2px] shadow-black",
+				className
+			)}
+		>
 			{children}
 		</div>
 	);
