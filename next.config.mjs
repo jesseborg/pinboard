11bae1e223f995f893c8240531a74dc98bb5637b
jesseborg <6882782+jesseborg@users.noreply.github.com>
2024-03-01 22:08:57 +1000

    change: remove million-lint

--- next.config.mjs
+++ next.config.mjs
@@ -1,5 +1,4 @@
-import MillionLint from "@million/lint";
 /** @type {import('next').NextConfig} */
 const nextConfig = {};
 
-export default MillionLint.next(nextConfig, { rsc: true });
+export default nextConfig;
