4e98f50b347a1a9a4162754a2c80f7eff60d933c
jesseborg <6882782+jesseborg@users.noreply.github.com>
2024-04-01 23:40:00 +1000

    feat(node): add basic edit dialog to `ImageNode`

--- src/components/nodes/image-node.tsx
+++ src/components/nodes/image-node.tsx
@@ -1,3 +1,12 @@
+import {
+	FormEvent,
+	MouseEvent,
+	PropsWithChildren,
+	useLayoutEffect,
+	useRef,
+	useState,
+} from "react";
+import { Button } from "../button";
 import { CustomNodeProps, NodeProps } from "../pinboard/types";
 import { BaseNode } from "./base-node";
 
@@ -10,10 +19,68 @@ export type ImageNodeProps = NodeProps & {
 };
 
 export function ImageNode({ node }: CustomNodeProps<ImageNodeProps>) {
+	const [editing, setEditing] = useState(false);
+
+	function handleEdit() {
+		setEditing(true);
+	}
+
 	return (
-		<BaseNode node={node}>
-			{/* eslint-disable-next-line @next/next/no-img-element */}
-			<img src={node.data.src} alt={node.data.alt} />
-		</BaseNode>
+		<>
+			<BaseNode node={node} handleEdit={handleEdit}>
+				{/* eslint-disable-next-line @next/next/no-img-element */}
+				<img src={node.data.src} alt={node.data.alt} />
+			</BaseNode>
+
+			{editing && (
+				<EditDialog open onClose={() => setEditing(false)}>
+					<div className="p-2">
+						<p>{node.id}</p>
+						<form method="dialog">
+							<Button>Save</Button>
+						</form>
+					</div>
+				</EditDialog>
+			)}
+		</>
+	);
+}
+
+type DialogProps = {
+	open?: boolean;
+	onClose?: () => void;
+};
+
+function EditDialog({
+	open = false,
+	onClose,
+	children,
+}: PropsWithChildren<DialogProps>) {
+	const ref = useRef<HTMLDialogElement>(null);
+
+	useLayoutEffect(() => {
+		ref.current?.showModal();
+	}, []);
+
+	function handleOnClick(event: MouseEvent<HTMLDialogElement>) {
+		if (event.target === ref.current) {
+			ref.current?.close();
+			onClose?.();
+		}
+	}
+
+	function handleSubmit(event: FormEvent<HTMLDialogElement>) {
+		event.preventDefault();
+	}
+
+	return (
+		<dialog
+			ref={ref}
+			onClick={handleOnClick}
+			onSubmit={handleSubmit}
+			className="rounded-md bg-white shadow-3xl backdrop:bg-black/50 pointer-events-auto"
+		>
+			{children}
+		</dialog>
 	);
 }
