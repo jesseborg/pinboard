40b7551196947476cb72d2b4fe1898c983e9cba3
jesseborg <6882782+jesseborg@users.noreply.github.com>
2024-04-05 13:30:08 +1000

    refactor(pinboard): move `NameContainer` into own file

--- src/components/pinboard/pinboard.tsx
+++ src/components/pinboard/pinboard.tsx
@@ -1,7 +1,6 @@
 "use client";
 
 import useDrag from "@/hooks/use-drag";
-import { cn } from "@/lib/utils";
 import {
 	Node,
 	Nodes,
@@ -11,17 +10,16 @@ import {
 import {
 	usePinBoardActions,
 	usePinBoardHydrated,
-	usePinBoardName,
 	usePinBoardXY,
 } from "@/stores/use-pinboard-store";
 import {
-	FocusEvent,
 	PropsWithChildren,
 	useCallback,
 	useEffect,
 	useRef,
 	useState,
 } from "react";
+import { NameContainer } from "./name-container";
 import { NodeHandle, NodeTypes } from "./types";
 
 type PinBoardProps = {
@@ -87,44 +85,6 @@ function DraggablePinBoard({
 			{children}
 			<NameContainer />
 			<NodesContainer {...props} />
-		</div>
-	);
-}
-
-function NameContainer() {
-	const name = usePinBoardName();
-	const { setName } = usePinBoardActions();
-
-	const [editing, setEditing] = useState(false);
-
-	function handleBlur(event: FocusEvent) {
-		setEditing(false);
-
-		if (window.getSelection()?.focusNode?.contains(event.target)) {
-			window.getSelection()?.empty();
-		}
-	}
-
-	return (
-		<div className="absolute flex w-full justify-center pt-6 z-50">
-			<input
-				autoComplete="off"
-				autoCapitalize="off"
-				autoCorrect="off"
-				readOnly={!editing}
-				spellCheck={editing}
-				maxLength={25}
-				className={cn(
-					"px-4 py-2 bg-white text-sm font-light w-64 shadow-sm text-center cursor-default outline-none",
-					{
-						"cursor-text outline outline-2": editing,
-					}
-				)}
-				defaultValue={name}
-				onChange={({ target: { value } }) => setName(value)}
-				onDoubleClick={() => setEditing(true)}
-				onBlur={handleBlur}
-			/>
 		</div>
 	);
 }
