1bd1128377fff7899ea41d9ae896e990d8f6e3ad
jesseborg <6882782+jesseborg@users.noreply.github.com>
2024-04-06 16:54:43 +1000

    feat: press enter to edit pinboard name

--- src/components/pinboard/name-container.tsx
+++ src/components/pinboard/name-container.tsx
@@ -1,15 +1,18 @@
+import { useKeyDown } from "@/hooks/use-keydown";
 import { cn } from "@/lib/utils";
 import {
 	usePinBoardActions,
 	usePinBoardName,
 } from "@/stores/use-pinboard-store";
-import { FocusEvent, useState } from "react";
+import { FocusEvent, useRef, useState } from "react";
 
 export function NameContainer() {
 	const name = usePinBoardName();
 	const { setName } = usePinBoardActions();
 
 	const [editing, setEditing] = useState(false);
+
+	const inputRef = useRef<HTMLInputElement | null>(null);
 
 	function handleBlur(event: FocusEvent) {
 		setEditing(false);
@@ -19,9 +22,22 @@ export function NameContainer() {
 		}
 	}
 
+	useKeyDown(inputRef, ["Enter", "Escape"], (key) => {
+		if (key === "Enter") {
+			setEditing(true);
+			return;
+		}
+
+		if (key === "Escape") {
+			setEditing(false);
+			return;
+		}
+	});
+
 	return (
 		<div className="absolute flex w-full justify-center pt-6 z-50">
 			<input
+				ref={inputRef}
 				autoComplete="off"
 				autoCapitalize="off"
 				autoCorrect="off"
@@ -29,7 +45,7 @@ export function NameContainer() {
 				spellCheck={editing}
 				maxLength={25}
 				className={cn(
-					"px-4 py-2 bg-white text-sm font-light w-64 shadow-sm text-center cursor-default outline-none",
+					"px-4 py-2 bg-white text-sm font-light w-64 shadow-sm text-center cursor-default outline-none focus:ring-2 ring-black",
 					{
 						"cursor-text outline outline-2": editing,
 					}
