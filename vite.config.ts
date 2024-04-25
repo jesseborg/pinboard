da293b177fe580b55ad6fbdc5cfcefdd463029c6
jesseborg <6882782+jesseborg@users.noreply.github.com>
2024-04-25 19:10:28 +1000

    chore: more vite config changes

--- vite.config.ts
+++ vite.config.ts
@@ -5,12 +5,14 @@ import { defineConfig } from "vite";
 // https://vitejs.dev/config/
 export default defineConfig({
 	resolve: {
-		alias: [{ find: "@", replacement: resolve(__dirname, "src") }],
+		alias: {
+			"@": resolve(__dirname, "./src"),
+		},
 	},
 	build: {
 		rollupOptions: {
 			input: {
-				main: resolve(__dirname, "src/index.html"),
+				main: resolve(__dirname, "./index.html"),
 			},
 		},
 	},
