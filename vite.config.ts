5b9da50f3b8f9ef580c03e74ef0fea90053dd202
jesseborg <6882782+jesseborg@users.noreply.github.com>
2024-04-25 18:59:22 +1000

    chore: move 'index.html' into 'src'

--- vite.config.ts
+++ vite.config.ts
@@ -12,7 +12,7 @@ export default defineConfig({
 	build: {
 		rollupOptions: {
 			input: {
-				main: resolve(__dirname, "index.html"),
+				main: resolve(__dirname, "src/index.html"),
 			},
 		},
 	},
