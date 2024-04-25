b6d44fb9b1a469807f94cf3da3a4333dfc80a8f9
jesseborg <6882782+jesseborg@users.noreply.github.com>
2024-04-25 18:45:28 +1000

    fix: add 'index.html' to vite config

--- vite.config.ts
+++ vite.config.ts
@@ -1,11 +1,19 @@
 import react from "@vitejs/plugin-react-swc";
+import { resolve } from "path";
 import { defineConfig } from "vite";
 
 // https://vitejs.dev/config/
 export default defineConfig({
 	resolve: {
 		alias: {
-			"@": "/src",
+			"@": resolve(__dirname, "src"),
+		},
+	},
+	build: {
+		rollupOptions: {
+			input: {
+				main: resolve(__dirname, "index.html"),
+			},
 		},
 	},
 	plugins: [react()],
