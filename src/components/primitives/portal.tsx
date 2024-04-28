caa1b06286e830d5d59826fb6fc1e150489c12c9
jesseborg <6882782+jesseborg@users.noreply.github.com>
2024-04-28 23:50:58 +1000

    feat: add forwardRef to `Portal` component

--- src/components/primitives/portal.tsx
+++ src/components/primitives/portal.tsx
@@ -1,4 +1,4 @@
-import { ComponentPropsWithoutRef } from "react";
+import { ComponentPropsWithoutRef, ElementRef, forwardRef } from "react";
 import { createPortal } from "react-dom";
 import { Slot } from "./slot";
 
@@ -6,11 +6,14 @@ export type PortalProps = ComponentPropsWithoutRef<"div"> & {
 	asChild?: boolean;
 	container?: HTMLElement | null;
 };
-export function Portal({
-	asChild = false,
-	container = globalThis?.document?.body,
-	...props
-}: PortalProps) {
-	const Component = asChild ? Slot : "div";
-	return container ? createPortal(<Component {...props} />, container) : null;
-}
+export const Portal = forwardRef<ElementRef<"div">, PortalProps>(
+	(
+		{ asChild = false, container = globalThis.document.body, ...props },
+		ref
+	) => {
+		const Component = asChild ? Slot : "div";
+		return container
+			? createPortal(<Component {...props} ref={ref} />, container)
+			: null;
+	}
+);
