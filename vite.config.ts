1cb10fea47724c496069c5498d78e365dd7b04b0
jesseborg <6882782+jesseborg@users.noreply.github.com>
2024-05-16 19:47:27 +1000

    chore: use react-compiler

--- vite.config.ts
+++ vite.config.ts
@@ -1,4 +1,4 @@
-import react from "@vitejs/plugin-react-swc";
+import react from "@vitejs/plugin-react";
 import { resolve } from "path";
 import { defineConfig } from "vite";
 
@@ -9,5 +9,11 @@ export default defineConfig({
 			"@": resolve(__dirname, "./src"),
 		},
 	},
-	plugins: [react()],
+	plugins: [
+		react({
+			babel: {
+				plugins: [["babel-plugin-react-compiler", {}]],
+			},
+		}),
+	],
 });
